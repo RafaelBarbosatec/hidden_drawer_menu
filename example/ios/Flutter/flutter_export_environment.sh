9eb2aed8b7a8de2f354d8fe1d0107ab7d99ddbb3
youssefali424 <youssef4242014@gmail.com>
2021-05-03 05:35:37 +0200

    add custom transition and animated background

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\sdk\flutter-sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\projects\flutter\hidden_drawer_menu\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\youss\Desktop\projects\flutter\hidden_drawer_menu\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_BUILD_NAME=2.0.0"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\sdk\flutter-sdk\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
